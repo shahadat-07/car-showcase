e445b7d322f5adf7ce9e38372f43ae737cd67bed
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2023-06-11 14:37:37 +0600

    hero,nav,footer section are completed

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,23 @@
+"use client";
+import { CustomButtonProps } from "@/types";
 import Image from "next/image";
-const CustomButton = () => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
-      className={`custome-btn`}
-      onClick={() => {}}
-    ></button>
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {
+        handleClick;
+      }}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
   );
 };
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import CustomButton from "./CustomButton";
+
 const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, or rent a car - quickly and easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
 
         <p className="hero__subtitle">
@@ -13,7 +24,18 @@ const Hero = () => {
           process.
         </p>
 
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
