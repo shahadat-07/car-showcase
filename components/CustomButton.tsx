b676ceffba5afc884cc97174c848c72c1ebf00bf
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2023-06-11 08:11:19 +0600

    building hero section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custome-btn`}
+      onClick={() => {}}
+    ></button>
+  );
+};
+
+export default CustomButton;
