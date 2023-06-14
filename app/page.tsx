65a40d1722affcef3e31a42f25652c8d804eca47
Shahadat Hossen <shahadat.hossen.web@gmail.com>
2023-06-14 06:33:30 +0600

    search functionality added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
-import { fetchCar } from "@/utils";
+import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default async function Home() {
-  const allCars = await fetchCar();
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
